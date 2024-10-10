8e22a9ab5c102b20212f0e75e658a14409e90637
MadhuGowda07 <22hr1a3216@mtieat.org>
2024-10-10 22:23:57 +0530

    Create Data_Types.py

---
+++ Data_Types.py
@@ -0,0 +1,30 @@
+#Setting the Data Type
+
+'''
+x = "Hello World"	str	
+x = 20	int	
+x = 20.5	float	
+x = 1j	complex	
+x = ["apple", "banana", "cherry"]	list	
+x = ("apple", "banana", "cherry")	tuple	
+x = range(6)	range	
+x = {"name" : "John", "age" : 36}	dict	
+x = {"apple", "banana", "cherry"}	set	
+x = True	bool		
+x = None	NoneType
+'''
+
+#Setting the Specific Data Type
+'''
+x = str("Hello World")	str	
+x = int(20)	int	
+x = float(20.5)	float	
+x = complex(1j)	complex	
+x = list(("apple", "banana", "cherry"))	list	
+x = tuple(("apple", "banana", "cherry"))	tuple	
+x = range(6)	range	
+x = dict(name="John", age=36)	dict	
+x = set(("apple", "banana", "cherry"))	set	
+x = bool(5)	bool	
+
+'''
