d9f7538f36fc45934a86edbc8c355460f2a53f43
MadhuGowda07 <22hr1a3216@mtieat.org>
2024-10-10 22:16:57 +0530

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,43 @@
+x = 5
+y = "John"
+print(x)  #5
+print(y)  #John
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)  #Sally(output)
+
+#Casting
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#Get the Type
+x = 5
+y = "John"
+print(type(x))  #<class 'int'>
+print(type(y))  #<class 'str'>
+
+#Case-Sensitive
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+#Illegal variable names
+2myvar = "John"
+my-var = "John"
+my var = "John"
+
+#multiple variables in one line
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#One Value to Multiple Variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#
