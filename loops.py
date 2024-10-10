1cb121e4f656113d88dd6eb0bcef349e2780ca52
MadhuGowda07 <22hr1a3216@mtieat.org>
2024-10-10 23:14:20 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,23 @@
+#while loop
+i = 1
+while i < 6:
+  print(i)
+  i += 1        
+'''
+1
+2
+3
+4
+5
+'''
+
+#for loop
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+'''
+apple
+banana
+cherry
+'''
